c14cfa50079d6f747e2f1be638276a1312080c6c
sofianeSghiouar <sofianesgh@gmail.com>
2021-09-25 18:41:05 +0200

    do not display modal, try to fix it

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { View, StyleSheet, ActivityIndicator } from 'react-native'
+import { View } from 'react-native'
 import List from './Views/List';
 
 
@@ -13,23 +13,4 @@ export default function App() {
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // alignItems: 'center',
-    // justifyContent: 'center'
-  },
-  textItem: {
-    padding: 15,
-    marginBottom: 10,
-    backgroundColor: '#e6e6fa',
-    fontSize: '1.5em',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  loading: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+
